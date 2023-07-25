b23f800e0b63c0e31353748a25a111bd89554413
batista.yuri <yuri.1115@hotmail.com>
2023-07-25 09:17:53 -0300

    build: add spring boot and maven files

---
+++ src/test/java/br/com/softexpert/desafiojavabackend/DesafioJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softexpert.desafiojavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
